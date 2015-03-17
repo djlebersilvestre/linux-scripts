0c0488b5d6d64bc180ab341fad88e9dc458600f3
Daniel Silvestre <djlebersilvestre@gmail.com>
2015-03-17 17:59:05 -0300

    Script to handle docker installation and configuration

---
+++ dkr-commands.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+dkr_install() {
+  if ! type -t update_pkgs | grep -i function > /dev/null; then
+    if [ -e "${0%/*}/pkg-helper.sh" ]; then
+      source "${0%/*}/pkg-helper.sh"
+    else
+      source <(curl https://raw.githubusercontent.com/djlebersilvestre/linux-scripts/master/pkg-helper.sh)
+    fi
+  fi
+
+  update_pkgs
+  install_pkgs 'docker.io'
+}
+
+dkr_setup() {
+  if groups | grep -q docker; then
+    echo 'Group docker already exists. Skipping.'
+  else
+    sudo groupadd docker
+    echo 'Group docker created.'
+  fi
+
+  if groups ${USER} | grep -q docker; then
+    echo '${USER} already associated to docker group. Skipping.'
+  else
+    sudo gpasswd -a ${USER} docker
+    echo '${USER} associated to docker group.'
+  fi
+
+  sudo service docker.io restart
+  newgrp docker
+  echo 'Please log out and log in so the changes can be applied.'
+}
